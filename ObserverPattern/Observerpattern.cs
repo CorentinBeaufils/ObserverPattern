837fe6403e0e879bc29bccdcc8d2d09c9658e60c
CorentinBeaufils <corentin.beaufils@viacesi.fr>
2023-11-26 14:24:17 +0100

    Add project files.

---
+++ ObserverPattern/Observerpattern.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    public interface IAtomObserver
+    {
+        void Update(AtomObservable atomObservable);
+    }
+
+    public interface IAtomObservable
+    {
+        void Attach(IAtomObserver observer);
+        void Detach(IAtomObserver observer);
+        void Notify();
+    }
+
+    public class AtomObserver : IAtomObserver
+    {
+        private string _ObserveNname;
+
+        public AtomObserver(string nname)
+        {
+            _ObserveNname = nname;
+            Console.WriteLine($"Création de l'observer {_ObserveNname}");
+        }
+
+        public void Update(AtomObservable atom)
+        {
+            Console.WriteLine($"{_ObserveNname} a été notifié du changement de l'atome {atom.Name}.");
+        }
+    }
+
+    public class AtomObservable : IAtomObservable
+    {
+        private List<IAtomObserver> observers = new List<IAtomObserver>();
+        private string name;
+        private string symbol;
+        private double mass;
+
+        public AtomObservable(string name, string symbol, double mass)
+        {
+            this.name = name;
+            this.symbol = symbol;
+            this.mass = mass;
+            Console.WriteLine($"Création de l'atom {name}, de symbole {symbol}, et de mass : {mass}");
+        }
+        public string Name => name;
+        public string Symbol => symbol;
+        public double Mass => mass;
+
+
+        public void Attach(IAtomObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IAtomObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(this);
+            }
+        }
+        public void ChangeState(string newName, string newSymbol, double newMass)
+        {
+            Console.WriteLine($"Update des Valeurs : \nname : {name} => {newName}, Symbole : {symbol}" +
+                $" => {newSymbol}, Mass : {mass} => {newMass}");
+            name = newName;
+            symbol = newSymbol;
+            mass = newMass;
+            
+            Notify();
+        }
+    }
+}
+
---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ObserverPattern;
+
+Console.WriteLine("Declaration des Objets : \n\n");
+AtomObservable atom = new AtomObservable("Carbon", "C", 12.001074);
+AtomObserver Obs1 = new AtomObserver("Observer1");
+AtomObserver Obs2 = new AtomObserver("Oberver2");
+
+Console.WriteLine("\n ------------------------------------ \n");
+
+atom.Attach(Obs1);
+atom.Attach(Obs2);
+
+Console.WriteLine("\nUpdate \n\n");
+atom.ChangeState("Hydrogen", "H", 1.00794);
+
